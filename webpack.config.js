d5b0a410c96583809e4e92bc81c6a255d4c9bbfd
EternalQuasar <70824102+EternalQuasar0206@users.noreply.github.com>
2022-02-22 09:19:12 -0300

    chore: configuration updates/include favico

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,7 @@ const config = {
   },
   plugins: [
     new HtmlWebpackPlugin({
+      favicon: "./public/favicon.ico",
       template: "./public/index.html",
       filename: "index.html",
       inject: "body",
